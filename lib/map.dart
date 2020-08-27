6ccc997e3d1ef418a2647e97e5e9c8f772ce380f
David <davidgarg28@gmail.com>
2020-08-27 20:22:37 +0530

    gooogle map initialised done

---
+++ lib/map.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapView extends StatefulWidget {
+  @override
+  _MapViewState createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        
+      ),
+      
+    );
+  }
+  }

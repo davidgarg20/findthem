c0658fb36f78797320c9d6ca3aa77f5a2b27caed
ftyf <rtd@yt.com>
2020-08-30 20:44:08 +0530

    Sidebar Menu Done

--- lib/map.dart
+++ lib/map.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class Map extends StatefulWidget {
+  @override
+  _MapState createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(child: Center(child: Text("Map Screen"),),),
+    );
+  }
+}
